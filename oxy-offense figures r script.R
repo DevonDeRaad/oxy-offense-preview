b85786fc8fdaf0257d75f3cfde6f23f3960d0172
Devon DeRaad <deraad@oxy.edu>
2016-09-26 22:29:37 -0700

    Update oxy-offense figures r script.R

--- oxy-offense figures r script.R
+++ oxy-offense figures r script.R
@@ -9,6 +9,16 @@ library(ggthemes)
 ###set color theme
 c<- c("mediumpurple4", "red4", "goldenrod", "sienna2", "seagreen", "royalblue4", "firebrick", "darkslateblue")
 ###create rushing yards graph
-ggplot(data=rush, aes(x=reorder(school, yds), y=yds, fill=school)) + theme_minimal(base_size=13,)+ ylab("Rushing Yards") + xlab("School")+ geom_bar(stat="identity")+ scale_fill_manual(values=c)+ theme(axis.text.x=element_text(angle=45, face="bold", hjust=1,vjust=1.5))
+ggplot(data=rush, aes(x=reorder(school, yds), y=yds, fill=school))+
+  theme_minimal(base_size=13,)+ 
+  ylab("Rushing Yards")+
+  xlab("School")+
+  geom_bar(stat="identity")+
+  scale_fill_manual(values=c)+
+  theme(axis.text.x=element_text(angle=45, face=c("plain", "plain", "plain","bold", "plain", "plain", "plain", "plain"), hjust=1,vjust=1.5))
 ###create td/int 2015 figure
-ggplot(data=pass, aes(x=reorder(name, -value), y=value, fill=result)) + theme_minimal(base_size=13,)+ geom_bar(aes(fill = result), position = "dodge", stat="identity")+ xlab("2015 SCIAC Quarterbacks")+ ylab("")+ theme(axis.text.x=element_text(angle=45, face="bold", hjust=1,vjust=1.35))
+ggplot(data=pass, aes(x=reorder(name, -value), y=value, fill=result))+
+  theme_minimal(base_size=13,)+
+  geom_bar(aes(fill = result), position = "dodge", stat="identity")+
+  xlab("2015 SCIAC Quarterbacks")+ ylab("")+
+  theme(axis.text.x=element_text(angle=45, face=c("bold""plain", "plain", "plain", "plain", "plain", "plain", "plain") hjust=1,vjust=1.35))
